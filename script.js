7e88c4bddb8f7e3f5cccadae4ba141acb47d7cba
Ho Thuy Tien <=>
2023-01-09 00:07:45 +0700

    ..

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-var myIndex = 0;
-    carousel();
-    
-function carousel() {
-var i;
-var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+
 function handleChangeHeader() {
   let changePosition = document.querySelector(".store").offsetTop;
   if(window.scrollY >= changePosition) {

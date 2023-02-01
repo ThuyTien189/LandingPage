7ee56b9edbc542412efb23e12bd56f1a5cf1fcdf
Ho Thuy Tien <=>
2023-02-01 17:54:12 +0700

    ...

--- script.js
+++ script.js
@@ -4,20 +4,39 @@ function handleChangeHeader() {
     document.querySelector(".header").classList.add("header-fixed");
     document.querySelector(".logo-normal").style.display = "none";
     document.querySelector(".logo-fixed").style.display = "block";
+    document.querySelector(".intro").style.paddingTop = "84px";
+    document.querySelector(".scrollUp").style.display = "block";
   }
   else {
     document.querySelector(".header").classList.remove("header-fixed");
     document.querySelector(".logo-normal").style.display = "block";
     document.querySelector(".logo-fixed").style.display = "none";
+    document.querySelector(".intro").style.paddingTop = "0";
+    document.querySelector(".scrollUp").style.display = "none";
   }
 }
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
+let isCounted = [true,true,true,true]
 function numberCounter(qSeclector, start = 0, end, duration = 1000) {
   let changePosition = document.querySelector(".number").offsetTop - window.innerHeight;
-  let isCounted = 0
   console.log(scrollY, changePosition)
   const target = document.querySelector(qSeclector)
-  if(isCounted == 0 && window.scrollY >= changePosition) {
+  if(isInViewport(target) && (isCounted.find(val => val === true))) {
+      for(let i = 0; i < isCounted.length; i++) {
+        if(isCounted[i] ) {
+          isCounted[i] = false;
+          break;
+        }
+      }
       let startTimestamp = null;
       const step = (timestamp) => {
       if (!startTimestamp) startTimestamp = timestamp;
@@ -27,7 +46,6 @@ function numberCounter(qSeclector, start = 0, end, duration = 1000) {
         window.requestAnimationFrame(step);
       }
       };
-    isCounted = 1
     window.requestAnimationFrame(step);
   }
 }
@@ -63,20 +81,25 @@ function slideShow() {
   });
 }
 $(document).ready(slideShow)
+
 window.addEventListener("scroll", handleChangeHeader) 
+
 window.addEventListener("scroll", () => {
   numberCounter("#number1", 0, 10, 1000)
   numberCounter("#number2", 0, 23, 1000)
   numberCounter("#number3", 0, 9, 1000)
   numberCounter("#number4", 0, 12, 1000)
 }, 
-  {once: true}
+  {passive: true}
 );
 AOS.init();
 
 const cb_showMenu = document.querySelector(".cb_showMenu");
 cb_showMenu.addEventListener("click", () => {
   if(cb_showMenu.checked) {
+    document.querySelector(".menu-tablet-icon").style.transform = "rotate(360deg)";
+    document.querySelector(".menu-tablet-icon").style.transitionDuration = ".3s";
+    document.querySelector(".nav-inner").classList.remove('nav-inner-hide-animation')
     document.querySelector(".menu-tablet").style.display = "block";
     $('body').css({overflow: "hidden"})
     cb_showMenu.checked = false; 
@@ -85,8 +108,11 @@ cb_showMenu.addEventListener("click", () => {
 const cb_hideMenu = document.querySelector(".cb_closeMenu");
   cb_hideMenu.addEventListener("click", () => {
   if(cb_hideMenu.checked) {
-    document.querySelector(".menu-tablet").style.display = "none";
+    setTimeout(() => {
+      document.querySelector(".menu-tablet").style.display = "none";
+    },"300")
     $('body').css({overflow: "unset"})
     cb_hideMenu.checked = false;
+    document.querySelector(".nav-inner").classList.add('nav-inner-hide-animation')
   } 
 })
